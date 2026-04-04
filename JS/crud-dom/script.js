d75092fb9bb8c1dc00f58a19e96963aea6f80f9c
Pablo Junior <xink.kr@egmail.com>
2026-04-04 01:38:14 -0300

    função abrir modal

--- JS/crud-dom/script.js
+++ JS/crud-dom/script.js
@@ -21,14 +21,27 @@ const bntSalvar = document.getElementById("salvar") //botao de salvar
 
 let livroEditando = null //informa que ao inicar nao tera edição acontencendo
 
-function abrirModal() { //função que abre o modal para adicionar ou editar
-    modal.classList.remove("hidden")
 
-    if(modo == "novo") {
+//Em resumo essa função abre o modal e dependendo do valor passado decide se esta cadastrando algo novo ou editando
+
+function abrirModal(modo = "novo", livro = null) { //função que abre o modal para adicionar ou editar          |se nao passar algum valor no modal ele abre como padrao para novo
+    modal.classList.remove("hidden") //faz o modal aparecer
+
+    if(modo == "novo") { //verifica o valor passado, se for "novo" vai para cadastro
         //preparar para cadastro do livro
-        
-    }else {
+        modalTitulo.innerText = "Novo Titulo" //troca o titulo do modal para o titulo do q vc esta cadastrando
+        tituloInput.value = "" //limpa o input titulo
+        autorInput.value = "" //limpa o input autor
+        anoInput.value = "" //limpa o input ano
+        livroEditando = null //significa cadastro novo
+
+    }else { //se o valor passado nao for "novo", vai para edição
         //preparar para edição do livro
+        modalTitulo.innerText = "Editar Livro" //muda o titulo do modal
+        tituloInput.value = livro.titulo //preenche o input com titulo do livro atual
+        autorInput.value = livro.autor //preenche o input com o autor atual
+        anoInput.value = livro.ano //preenche o input com o ano atual
+        livroEditando = livro // guarda o livro que esta editando na variavel
     }
 }
 
@@ -36,6 +49,6 @@ function fecharModal() { //função para fechar o modal
     modal.classList.add("hidden")
 }
 
-bntNovo.addEventListener("click",() => abrirModal("novo"))
+bntNovo.addEventListener("click",() => abrirModal("novo")) //quando o usuario clicar em Novo Livro vai para abrir modal
 bntCancelar.addEventListener("click", fecharModal)
 
